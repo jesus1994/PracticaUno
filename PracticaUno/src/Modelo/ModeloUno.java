52082ed545fa266cee30589c64a61e52573c4e8b
jesus <jesus@192.168.1.214>
2015-02-26 13:56:55 +0100

    al leer la variable hola vemos si supera el maximo o el minimo

--- PracticaUno/src/Modelo/ModeloUno.java
+++ PracticaUno/src/Modelo/ModeloUno.java
@@ -1,10 +1,16 @@
 package Modelo;
 
+import java.util.Scanner;
+
 public class ModeloUno {
 	public static void main(String[] args) {
-		int hola;
+		Scanner leer=new Scanner(System.in);
+		int hola=leer.nextInt();
 		int min=0;
 		int max=20;
-		
+		if(hola>max||hola<min)
+			System.out.println("de eso na");
+		else
+			System.out.println("eso si");
 	}
 }
